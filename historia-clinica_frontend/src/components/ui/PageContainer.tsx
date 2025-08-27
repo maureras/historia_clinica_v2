154e25d74b32301aacdea02128bb28b2374170ea
maureras <mauro.galarraga@gmail.com>
2025-08-26 21:35:41 -0500

    Delete historia-clinica_frontend directory

--- historia-clinica_frontend/src/components/ui/PageContainer.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const PageContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <div className="min-h-screen bg-slate-50 p-4 font-sans">{children}</div>
-);
