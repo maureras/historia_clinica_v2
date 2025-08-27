c9a73d3ce0dd874889f7e8c64d48d0f40fb03f29
maureras <mauro.galarraga@gmail.com>
2025-08-26 21:35:30 -0500

    Delete historia-clinica_backend directory

--- historia-clinica_backend/src/common/http.ts
+++
@@ -1,4 +0,0 @@
-import type { Response } from "express";
-export const ok   = (res: Response, data: unknown) => res.status(200).json({ ok: true, data });
-export const created = (res: Response, data: unknown) => res.status(201).json({ ok: true, data });
-export const noContent = (res: Response) => res.status(204).send();
--- historia-clinica_backend/src/modules/auth/index.ts
+++
@@ -1,5 +0,0 @@
-export { AuthService } from './auth.service';
-export { AuthController } from './auth.controller';
-export { authRouter } from './auth.routes';
-export { requireAuth, requireRoles, optionalAuth } from './auth.middleware';
-export type { LoginRequest, LoginResponse, AuthUser, JWTPayload } from './auth.types';
